58c49cda5c6611439dd538e7815de4f715f628dc
Nepsaco <tobie.tsuzuki@gmail.com>
2020-04-17 17:53:08 -0600

    initial test test working needed async

--- brew_frontend/tests/unit/App.spec.js
+++ brew_frontend/tests/unit/App.spec.js
@@ -1,18 +1,21 @@
+import Vue from 'vue'
 import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
 import App from '@/App.vue'
 
 describe('App', () => {
-    it('hides h1 if a user is logged out', () => {
-        const wrapper = shallowMount(App)
-        wrapper.setData({ loggedIn: false })
+    const wrapper = shallowMount(App)
+    it('hides h1 if a user is logged out by default', async () => {
+        await Vue.nextTick()
+
         expect(wrapper.find('h1').isVisible()).to.equal(false)
     })
 
 
-    it('shows h1 if a user is logged in', () => {
-        const wrapper = shallowMount(App)
+    it('shows h1 if a user is logged in', async () => {
         wrapper.setData({ loggedIn: true })
+        await Vue.nextTick()
+
         expect(wrapper.find('h1').isVisible()).to.equal(true)
     })
 
